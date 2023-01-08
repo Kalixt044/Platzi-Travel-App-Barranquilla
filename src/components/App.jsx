c10ff4aae9adb52f74b25421d97e71384e1ccaf2
Antonio Gonzalez <agonzalezf87@gmail.com>
2023-01-08 13:41:01 -0600

    [ADD] Images and svg's, font, tailwind colors & font configuration

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold text-blue-600">
+    <h1 className="text-3xl font-bold text-terciary">
       Hello World!
     </h1>
   )
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,38 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'bali': "url('./src/assets/img/bali.jpg')",
+        'chicago': "url('./src/assets/img/chicago.jpg')",
+        'europe': "url('./src/assets/img/europe.jpg')",
+        'iceland': "url('./src/assets/img/iceland.jpg')",
+        'lA': "url('./src/assets/img/LA.jpg')",
+        'miami': "url('./src/assets/img/miami.jpg')",
+        'newYork': "url('./src/assets/img/new_york.jpg')",
+        'norway': "url('./src/assets/img/norway.jpg')",
+        'sanfrancisco': "url('./src/assets/img/sanFrancisco.jpg')",
+        'sanfranciscoDesktop': "url('./src/assets/img/sanFranciscoDesktop.jpg')",
+        'seattle': "url('./src/assets/img/seattle.jpg')",
+        'switzerland': "url('./src/assets/img/switzerland.jpg')",
+        'sydney': "url('./src/assets/img/sydney.jpg')",
+        'yosemite': "url('./src/assets/img/yosemite.jpg')",
+      },
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        'primary': "#cc2d4a",
+        'secondary': "#8fa206",
+        'terciary': "#61aec9"
+      }),
+      textColor: {
+        'primary': "#cc2d4a",
+        'secondary': "#8fa206",
+        'terciary': "#61aec9"
+      },
+      fontFamily: {
+        Montserrat: ['Montserrat', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
