9954bcd51521f64514600fb15be2f374a9bf75dc
Antonio Gonzalez <agonzalezf87@gmail.com>
2023-01-08 15:16:25 -0600

    [ADD] Suggestions card

--- src/templates/Suggestions.jsx
+++ src/templates/Suggestions.jsx
@@ -1,6 +1,14 @@
 const Suggestions = () => {
   return (
-    <div>Suggestions</div>
+    <div className="p-20">
+      <div className="w-48 h-64 shadow-md rounded-lg">
+        <div className="w-full h-3/5 rounded-t-lg bg-norway bg-cover"></div>
+        <div className="w-full h-2/5 rounded-b-lg p-4 bg-secondary">
+          <p className="text-white font-bold text-lg mb-2">Norway</p>
+          <p className="text-white text-sm">Beautiful Landscapes</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
