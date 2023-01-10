2e89e560d2ccdfbeaf518176c9e0b146ac73c2a7
Antonio Gonzalez <agonzalezf87@gmail.com>
2023-01-10 16:17:23 -0600

    [FEATURE] Featured cards variations. Fixed nesting styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,26 @@
-const Card = ({photo, name, description}) => {
+const Card = ({photo, name, description, featured, details}) => {
   const cardBg = {
     backgroundImage: `url('./src/assets/img/${photo}.jpg')`
   }
 
-  return (
-    <div className="Card">
-      <div style={cardBg}></div>
-      <div>
-        <p>{name}</p>
-        <p>{description}</p>
+  if (!featured) {
+    return (
+      <div className="Card">
+        <div style={cardBg}></div>
+        <div>
+          <p>{name}</p>
+          <p>{description}</p>
+        </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div className="Card__featured" style={cardBg}>
+        <p>{name}</p>
+        <p>{details}</p>
+      </div>
+    )
+  }
 }
 
 export { Card }
