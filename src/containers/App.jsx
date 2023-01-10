1bab64340f4266dd48dc22e94a47885c8423eb7b
Antonio Gonzalez <agonzalezf87@gmail.com>
2023-01-10 17:02:28 -0600

    [ADD] FAQs section, pending styling

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -3,6 +3,7 @@ import { TabBar } from "../components/TabBar"
 import { Hero } from "./Hero"
 import { Suggestions } from "./Suggestions"
 import { Featured } from "./Featured"
+import { Faqs } from "./Faqs"
 import { Footer } from "./Footer"
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
         <Hero />
         <Suggestions />
         <Featured />
+        <Faqs />
       </section>
       <Footer />
     </>
---
+++ src/containers/Faqs.jsx
@@ -0,0 +1,41 @@
+const Faqs = () => {
+  return (
+    <div className="w-full h-full" id="FAQS">
+      <div className="w-full h-full px-6 pt-6">
+        <p className="Section-title pb-6">FAQs</p>
+        <div>
+          <p>Cancelation Policy</p>
+          <p>
+            For stays of less than a week it is important to notify 3 days in advance, otherwise it will be penalized.
+          </p>
+        </div>
+        <div>
+          <p>Payment Methods</p>
+          <p>
+            We accept different payment methods: VISA, MasterCard, American Express, Paypal and Binance.
+          </p>
+        </div>
+        <div>
+          <p>Pets</p>
+          <p>
+            The subject of pets will depend directly on the host.
+          </p>
+        </div>
+        <div>
+          <p>Security Information</p>
+          <p>
+            All our rooms have insurance in case of accidents.
+          </p>
+        </div>
+        <div>
+          <p>Long Stays</p>
+          <p>
+            We have stays of up to 3 months, in this case an advance payment of 50% of the rental value is required.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export { Faqs }
