82ec243d4796094fe52ece93efa5b021ef223696
Antonio Gonzalez <agonzalezf87@gmail.com>
2023-01-11 14:03:55 -0600

    [UPDATE] Hero section responsive

--- src/containers/Hero.jsx
+++ src/containers/Hero.jsx
@@ -6,12 +6,15 @@ const Hero = () => {
   return (
     <div id="Hero">
       <div className="w-full h-3/4">
-        <div className="w-full h-full flex flex-col absolute space-y-96 py-4 items-center">
+        <div className="w-full h-full flex flex-col absolute space-y-96 py-4 items-center lg:space-y-0 lg:justify-start lg:items-start lg:pt-48">
           <SearchBar name="San Francisco"/>
+          <div className="hidden h-auto w-2/3 lg:flex pb-6">
+            <p className="text-4xl ml-16 font-bold text-white">Find more destinations</p>
+          </div>
           <Button title="Explore" />
         </div>
-        <div className="w-full h-full">
-          <img src={sanFrancisco} alt="San Francisco" />
+        <div className="w-full h-full lg:h-96 lg:bg-sanFranciscoDesktop lg:bg-cover lg:bg-center">
+          <img className="lg:hidden" src={sanFrancisco} alt="San Francisco" />
         </div>
       </div>
     </div>
