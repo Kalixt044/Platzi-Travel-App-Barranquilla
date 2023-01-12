78c44b0b87245b0d5fe13dc26d6304015c5c4fe3
Antonio Gonzalez <agonzalezf87@gmail.com>
2023-01-12 08:56:16 -0600

    [UPDATE] Footer & about dark styling

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -2,7 +2,7 @@ import { About } from "./About"
 
 const Footer = () => {
   return (
-    <footer className="w-full h-auto bg-gray-100 p-6">
+    <footer className="w-full h-auto bg-gray-100 p-6 dark:bg-gray-500">
       <About />
     </footer>
   )
