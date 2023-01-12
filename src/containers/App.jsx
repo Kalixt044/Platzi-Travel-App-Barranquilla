2a7fc9f42c9f7047e47eb2b4294bb45c3b5da2cd
Antonio Gonzalez <agonzalezf87@gmail.com>
2023-01-12 11:22:39 -0600

    [FEATURE] Use browser to initialize dark/light theme

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -3,7 +3,7 @@ import { ThemeContext } from "../context/ThemeContext"
 import { Home } from "../pages/Home"
 
 const App = () => {
-  const [theme, setTheme] = useState('Light')
+  const [theme, setTheme] = useState(!!window.matchMedia('(prefers-color-scheme:dark)').matches ? 'Dark' : 'Light')
   return (
     <ThemeContext.Provider value={{theme, setTheme}}>
       <Home />
