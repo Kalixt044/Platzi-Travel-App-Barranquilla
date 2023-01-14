3c3a2715af2656d593f811b0a5489450e49ab122
Antonio Gonzalez <agonzalezf87@gmail.com>
2023-01-14 10:27:36 -0600

    [FIX] Tablets breakpoint design

--- src/containers/Hero.jsx
+++ src/containers/Hero.jsx
@@ -4,16 +4,16 @@ import { Button } from '../components/Button'
 const Hero = () => {
   return (
     <div className="lg:mt-12" id="Hero">
-      <div className="w-full h-3/4">
+      <div className="w-full h-3/4 relative">
         <div className="Hero">
           <SearchBar name="San Francisco"/>
-          <div className="hidden h-auto w-2/3 lg:flex pb-6">
-            <p className="text-4xl ml-16 font-bold text-white">Find more destinations</p>
+          <div className="hidden h-auto w-full sm:flex pb-6">
+            <p className="text-4xl ml-16 font-bold text-primary">Find more destinations</p>
           </div>
           <Button title="Explore" />
         </div>
-        <div className="w-full h-full lg:h-96 lg:bg-[url('./assets/img/sanFranciscoDesktop.jpg')] lg:bg-cover lg:bg-center">
-          <img className="lg:hidden" src="./assets/img/sanFrancisco.jpg" alt="San Francisco" />
+        <div className="w-full h-full sm:h-72 lg:h-96 sm:bg-[url('./assets/img/sanFranciscoDesktop.jpg')] sm:bg-cover sm:bg-center">
+          <img className="sm:hidden" src="./assets/img/sanFrancisco.jpg" alt="San Francisco" />
         </div>
       </div>
     </div>
